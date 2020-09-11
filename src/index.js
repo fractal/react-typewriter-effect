ff0beeccee16c90a587510a242f0b2605ea68982
Roshan Padaki <roshanpadaki@gmail.com>
2020-09-11 17:42:44 -0400

    logging

--- src/index.js
+++ src/index.js
@@ -93,6 +93,8 @@ class TypeWriterEffect extends Component {
           this.props.multiText
             ? await this.multiTextDisplay(this.props.multiText)
             : await this.runAnimation(this.props.text);
+          console.log(this.props.multiText());
+          console.log(this.state.loop);
         } while (this.state.loop);
 
         this.props.hideCursorAfterText &&
